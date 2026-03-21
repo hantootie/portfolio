dc64b3abd6ded5e0e1e6684cee963f42ca177719
Bryan Liu <bryliu@amazon.com>
2026-03-21 01:16:06 -0400

    Add logo

--- src/pages/Landscape/Layout/SideNav/SideNav.tsx
+++ src/pages/Landscape/Layout/SideNav/SideNav.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 const NavButton: React.FC<{ onClick?: () => void; children: React.ReactNode }> = ({ onClick, children }) => (
     <button
         onClick={onClick}
-        className=" hover:opacity-70 hover:-translate-y-0.25 transition-transform duration-200 cursor-pointer"
+        className="hover:opacity-70 hover:-translate-y-0.25 transition-transform duration-200 cursor-pointer"
     >
         {children}
     </button>
@@ -13,14 +13,20 @@ const SideNav: React.FC = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="flex w-1/5 mt-10 mb-10">
-            <div className="flex flex-col gap-15 rock-salt-text text-2xl flex-1">
-                <NavButton onClick={() => navigate('/')}>Logo</NavButton>
+        <div className="flex w-1/5 mb-10">
+            <div className="flex flex-col gap-15 rock-salt-text text-sm md:text-lg lg:text-xl xl:text-2xl flex-1">
+                <div className="flex justify-center">
+                    <img 
+                        src="src/assets/Chae_logo.png" 
+                        onClick={() => navigate('/')}
+                        className=" w-3/4 mt-5 cursor-pointer hover:opacity-70 hover:-translate-y-0.25"
+                    />
+                </div>
                 <NavButton onClick={() => navigate('/work')}>Work .</NavButton>
                 <NavButton onClick={() => navigate('/contact')}>Contact</NavButton>
                 <NavButton>About</NavButton>
             </div>
-            <div className="w-[3px] bg-black rounded-full self-stretch" />
+            <div className="mt-10 w-[2.5px] bg-black rounded-full self-stretch" />
         </div>
     )
 }

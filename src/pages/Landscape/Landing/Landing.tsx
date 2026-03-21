c7b521ee31b7268636e26bff335502ec9efa4161
Bryan Liu <bryliu@amazon.com>
2026-03-20 23:39:43 -0400

    Landing + styling

--- src/pages/Landscape/Landing/Landing.tsx
+++ src/pages/Landscape/Landing/Landing.tsx
@@ -1,8 +1,15 @@
 
 const Landing: React.FC = () => {
     return (
-        <div className="h-full w-3/4">
-            Landing
+        <div className="h-full w-full rock-salt-text text-3xl">
+            <div className="ml-25 mt-25 h-30 flex flex-col justify-between">
+                <div>
+                    {"Hello :)"}
+                </div>
+                <div>
+                    Welcome to my portfolio.
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Landscape/SideNav/SideNav.tsx
+++ src/pages/Landscape/SideNav/SideNav.tsx
@@ -1,14 +1,21 @@
-
 const SideNav: React.FC = () => {
     return (
-        <div className="flex flex-col w-1/4">
-            <div>Logo</div>
-            <button>
-                Work
-            </button>
-            <button>
-                Contact
-            </button>
+        <div className="flex w-1/5 mt-10 mb-10">
+            <div className="flex flex-col gap-15 rock-salt-text text-2xl flex-1">
+                <button>
+                    Logo
+                </button>
+                <button>
+                    Work .
+                </button>
+                <button>
+                    Contact
+                </button>
+                <button>
+                    About
+                </button>
+            </div>
+            <div className="w-[3px] bg-black rounded-full self-stretch" />
         </div>
     )
 }

fa8dd07d2e2d0659f532084ba3e13592d7305b52
Bryan Liu <bryliu@amazon.com>
2026-03-28 01:39:55 -0400

    Mobile about

--- src/pages/Mobile/MobileAbout/MobileAbout.tsx
+++ src/pages/Mobile/MobileAbout/MobileAbout.tsx
@@ -1,8 +1,24 @@
+import FadeImage from "../../../components/FadeImage/FadeImage";
+import mobileAboutImage from "../../../assets/Mobile_About.png";
+import { NavButton } from "../../Landscape/Layout/SideNav/SideNav";
+import { useNavigate } from "react-router-dom";
 
 const MobileAbout: React.FC = () => {
-    return (
-        <div>
+    const navigate = useNavigate();
 
+    return (
+        <div className="relative">
+            <div className="absolute h-1/4 w-3/8 bottom-1/8 left-1/45 flex justify-center items-center">
+                <NavButton onClick={() => navigate('/work/resume')} className="rock-salt-text text-3xl text-red">
+                    Resume
+                    <br/>
+                    +
+                    <br/>
+                    CV
+                </NavButton>
+
+            </div>
+            <FadeImage src={mobileAboutImage} />
         </div>
     )
 }
